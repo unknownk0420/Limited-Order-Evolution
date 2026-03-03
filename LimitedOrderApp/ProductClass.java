7cca853d65cb8640083bd4c1340820c3d012a24e
Hash <unknownk0420@gmail.com>
2026-03-03 18:27:40 +0900

    refactor : 재고가 있는 경우 출력 방식을 수정과 인터페이스 추가
    - 확장성을 고려하여 다형성 즉 인터페이스를 추가를 하여 Product 클래스를 수정을 하지 않고 확장 할 수 있도록 설계.

--- LimitedOrderApp/ProductClass.java
+++ LimitedOrderApp/ProductClass.java
@@ -3,17 +3,23 @@ public class Product {
     private String productName;
     private int productPrice;
     private int productQuantity;
+    private DiscountPolicy discountPolicy;
 
-    public Product(String productName, int productPrice, int productQuantity) {
+    public Product(String productName, int productPrice, int productQuantity, DiscountPolicy discountPolicy) {
         this.productName = productName;
-        this.productPrice = Math.max(productPrice, 0);//private와 Math.max를 활용하여 외부의 잘못된 조작으로부터 데이터를 보호
+        this.productPrice = Math.max(productPrice, 0);
         this.productQuantity = productQuantity;
+        this.discountPolicy = discountPolicy;
     }
 
     public void order() {
+        int discount = discountPolicy.calculateDiscount(this.productPrice);
+        int finalPrice = this.productPrice - discount;
+
         if (this.productQuantity > 0) {
             this.productQuantity--;
-            System.out.println(this.productName + "주문 성공! 현재 재고: " + this.productQuantity);
+            System.out.println(this.productName + " 주문 성공! 현재 재고: " + this.productQuantity);
+            System.out.println("최종 결제 금액: " + finalPrice);
         } else {
             System.out.println(this.productName + "이 품절 되었습니다.");
         }
