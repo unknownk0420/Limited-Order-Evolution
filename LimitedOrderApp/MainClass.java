253913b5af81492d200aa6a59b9cab9c56568cf5
Hash <unknownk0420@gmail.com>
2026-03-03 19:54:34 +0900

    refactor : 변경 전 코드와 컬렉션(List) 도입 및 초기 데이터 구성
    - 대규모 데이터를 처리하기 위해 컬렉션(List) 도입
    - 사용자의 편의를 위해 초기 데이터 구성
    - 변경 전의 코드와 변경 후의 코드를 보여주기 위해 변경 전 코드를 주석으로 처리

--- LimitedOrderApp/MainClass.java
+++ LimitedOrderApp/MainClass.java
@@ -19,3 +19,26 @@ public class Main {
         }
     }
 }
+
+/*
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String productName = st.nextToken();
+        int productPrice = Integer.parseInt(st.nextToken());
+        int productQuantity = Integer.parseInt(st.nextToken());
+
+        Product product = new Product(productName, productPrice, productQuantity);
+
+        product.order();
+
+    }
+}
+*/
